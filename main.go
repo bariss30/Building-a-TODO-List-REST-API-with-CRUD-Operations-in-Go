e9e68c2e16f7e33b6557038b9c44710347f87a98
Barış Savak <128433645+bariss30@users.noreply.github.com>
2024-05-13 15:31:53 +0300

    Update main.go
    İki adet kullanıcı tipi belirlenmiştir
    Eğer kullanıcı bilgileri doğruysa jwt oturum anahtarı alması sağlanmıştır

--- main.go
+++ main.go
@@ -1,22 +1,95 @@
-// main.go
-
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
 )
 
-func main() {
-    // Ana HTTP yönlendirici oluşturma
-    http.HandleFunc("/", handler)
-
-    // Sunucuyu başlatma
-    fmt.Println("Server is running on http://localhost:8080")
-    http.ListenAndServe(":8080", nil)
+// Kullanıcı modeli
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Role     string `json:"role"` // Kullanıcı rolü (admin, user1 veya user2)
 }
 
-// handler fonksiyonu, gelen HTTP isteklerini işler
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, World!")
+// Anahtar
+var jwtKey = []byte("my_secret_key")
+
+// Kullanıcıları saklayan bir dilim (slice)
+var users = []User{
+	{Username: "user1", Password: "password1", Role: "user1"},
+	{Username: "user2", Password: "password2", Role: "user2"},
+	{Username: "admin", Password: "adminpass", Role: "admin"},
+}
+
+// Token yapısı
+type Claims struct {
+	Username string `json:"username"`
+	Role     string `json:"role"`
+	jwt.StandardClaims
+}
+
+// Kullanıcının JWT'sini oluşturur
+func generateJWT(username, role string) (string, error) {
+	expirationTime := time.Now().Add(5 * time.Minute)
+	claims := &Claims{
+		Username: username,
+		Role:     role,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+// Login işlemi
+func login(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	for _, u := range users {
+		if u.Username == user.Username && u.Password == user.Password {
+			token, err := generateJWT(u.Username, u.Role)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			response := fmt.Sprintf("Hoşgeldin, %s! Token: %s", u.Role, token)
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(response))
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusUnauthorized)
+	w.Write([]byte("Geçersiz kullanıcı adı veya şifre"))
+}
+
+// Ana sayfa
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Ana Sayfa")
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/login", login).Methods("POST")
+	r.HandleFunc("/home", homePage).Methods("GET")
+
+	fmt.Println("Server listening on port 8080...")
+	http.ListenAndServe(":8080", r)
 }
